d6606c9918e0b517d75c13d66f2a19c85cc0de0b
danoxv <denisiv.andreevi@mail.ru>
2022-11-22 11:28:43 +0300

    Added 2 new functions to work with strings.🤫

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -9,14 +9,19 @@ class Str extends \Illuminate\Support\Str
     /**
      * Replace text within a portion of a string.
      *
-     * @param  string|string[]  $string
-     * @param  string|string[]  $replace
-     * @param  int|int[]  $offset
-     * @param  int|int[]|null  $length
+     * @param string|string[] $string
+     * @param string|string[] $replace
+     * @param int|int[] $offset
+     * @param int|int[]|null $length
      * @return string|string[]
      */
     public static function substrReplace($string, $replace, $offset = 0, $length = null)
     {
         return UTF8::substr_replace($string, $replace, $offset, $length);
+    }
+
+    public static function after($subject, $search): string
+    {
+        return UTF8::str_substr_after_first_separator($subject, $search);
     }
 }
