5e52f428a015bc4623c0177ec97812a81dcbcda3
danoxv <denisiv.andreevi@mail.ru>
2022-11-22 11:21:55 +0300

    Added 2 new functions to work with strings.🤫

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -6,11 +6,27 @@ use voku\helper\UTF8;
 
 class Str
 {
-    public function strlen(string $string): int
+    public function strLen(string $string): int
     {
         return UTF8::strlen($string);
     }
-}
 
-$str = new Str();
-$str->strlen('Привет');
+    public function strToUpper(string $string): string
+    {
+        return UTF8::strtoupper($string);
+    }
+
+    public function ascii($value, $language = 'en'): string
+    {
+        return UTF8::to_ascii((string)$value, $language);
+    }
+
+    public function finish(string $value, string $cap): string
+    {
+        return UTF8::str_ensure_right($value, $cap);
+    }
+    public function firstChar(string $string): string
+    {
+        return UTF8::first_char($string);
+    }
+}
